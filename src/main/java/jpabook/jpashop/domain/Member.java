08e268bfd6e6ceb05cacce0447c0c8a1bcbc5a18
서규범 <sgb8170@naver.com>
2022-08-20 00:15:18 +0900

    값 타입 매핑

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -14,11 +14,10 @@ public class Member {
 
     private String name;
 
-    private String city;
+    //값 타입
+    @Embedded
+    private Address address;
 
-    private String street;
-
-    private String zipcode;
 
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
@@ -39,27 +38,11 @@ public class Member {
         this.name = name;
     }
 
-    public String getCity() {
-        return city;
+    public Address getAddress() {
+        return address;
     }
 
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getZipcode() {
-        return zipcode;
-    }
-
-    public void setZipcode(String zipcode) {
-        this.zipcode = zipcode;
+    public void setAddress(Address address) {
+        this.address = address;
     }
 }
